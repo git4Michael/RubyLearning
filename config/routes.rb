f9f533a277bee1558e42df96c2b25d2ed2bd8189
git4Michael <Michael.v.hanna@icloud.com>
2017-06-12 13:39:13 -0700

    played with routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get 'about-me', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
+  get 'pages/contact'
 
   resources :blogs
-  resources :portfolios
+
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
 end
